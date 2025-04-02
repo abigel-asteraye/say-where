3447e5a43ff474ba5b08721650437068b1f5cce8
abigel-asteraye <aster22a@mtholyoke.edu>
2025-04-02 14:33:21 -0400

    encrypting pws works

--- src/app/api/createUser.ts
+++ src/app/api/createUser.ts
@@ -1,10 +1,14 @@
+import { hash } from "crypto";
 import connectDB from "../../backend/db";
 import User from "../../backend/models/Users";
+import bcrypt from "bcrypt";
 
 export const createUser = async (name:String, email:string, password:string) => {
     await connectDB();
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    const user = new User({name, email, password})
+    const user = new User({name, email, password:hashedPassword});
     await user.save();
     return user;
 };
